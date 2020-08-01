0925b9c6748b15a66451a0258451cc5303bd7fcd
Michael <mkaufmann421@gmail.com>
2020-08-01 17:54:08 -0400

    Wait until typing is complete to display

--- pages/components/command.js
+++ pages/components/command.js
@@ -1,36 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Typist from "react-typist";
 import { Box, Text, SimpleGrid } from "@chakra-ui/core";
 export const Command = ({ command }) => {
+  const [finishedTyping, setFinishedTyping] = useState(false);
   return (
-    <Box w="full">
+    <Box>
       <Text my={0} fontWeight={500} fontSize="sm" fontFamily="monospace">
         <Box display="flex" flexDirection="row" alignItems="center">
           <span style={{ color: "#fff", "padding-right": "5px" }}>
             {`⭢ ${command.dir}`}
           </span>
-          <Typist>
+          <Typist onTypingDone={() => setFinishedTyping(true)}>
             <span style={{ color: "#ffcb05" }}>{command.command}</span>
           </Typist>
         </Box>
       </Text>
-
-      <SimpleGrid columns={2}>
-        {command.output.map((chunk) => {
-          return (
-            <Text
-              key={chunk}
-              my={0}
-              color="white"
-              fontSize="sm"
-              fontWeight="500"
-              fontFamily="monospace"
-            >
-              {chunk}
-            </Text>
-          );
-        })}
-      </SimpleGrid>
+      {!!finishedTyping && (
+        <SimpleGrid columns={2} w="full">
+          {command.output.map((chunk) => {
+            return (
+              <Text
+                key={chunk}
+                my={0}
+                color="white"
+                fontSize="sm"
+                fontWeight="500"
+                fontFamily="monospace"
+              >
+                {chunk}
+              </Text>
+            );
+          })}
+        </SimpleGrid>
+      )}
     </Box>
   );
 };
--- pages/components/window.js
+++ pages/components/window.js
@@ -64,7 +64,7 @@ export const Window = ({ title }) => {
           textAlign="left"
           p={1}
         >
-          {commands.map((command) => {
+          {commands.map((command, index) => {
             return <Command key={command.command} command={command} />;
           })}
 
