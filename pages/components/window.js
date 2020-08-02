6e508d94258895a19c0dc8ecbe8fa61b0c710eb8
Michael <mkaufmann421@gmail.com>
2020-08-01 20:56:39 -0400

    Add basic resize

--- pages/components/window.js
+++ pages/components/window.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Box, Text, SimpleGrid } from "@chakra-ui/core";
-import Draggable from "react-draggable";
-import Typist from "react-typist";
+import { Rnd } from "react-rnd";
 import { Command } from "./command";
 export const Window = ({ title }) => {
   const commands = [
@@ -22,59 +21,53 @@ export const Window = ({ title }) => {
     },
   ];
   return (
-    <Draggable
-      handle=".window-handle"
-      defaultPosition={{
-        x: 0,
-        y: 0,
-      }}
+    <Rnd
+      dragHandleClassName="window-handle"
+      size={{ width: "300px", height: 300 }}
     >
-      <Box w="500px">
+      <Box
+        borderTopLeftRadius="0.25rem"
+        borderTopRightRadius="0.25rem"
+        bg="black"
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        className="window-handle"
+      >
         <Box
-          borderTopLeftRadius="0.25rem"
-          borderTopRightRadius="0.25rem"
-          bg="black"
           display="flex"
           flexDirection="row"
           alignItems="center"
-          justifyContent="space-between"
-          className="window-handle"
+          position="absolute"
         >
-          <Box
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            position="absolute"
-          >
-            <Box bg="#ff605c" borderRadius="10rem" w="10px" h="10px" mx={1} />
-            <Box bg="#ffbd44" borderRadius="10rem" w="10px" h="10px" mr={1} />
-            <Box bg="#00ca4e" borderRadius="10rem" w="10px" h="10px" mr={1} />
-          </Box>
-          <Box />
-          <Text color="white" fontFamily="monospace" my={1} userSelect="none">
-            {title}
-          </Text>
-          <Box />
+          <Box bg="#ff605c" borderRadius="10rem" w="10px" h="10px" mx={1} />
+          <Box bg="#ffbd44" borderRadius="10rem" w="10px" h="10px" mr={1} />
+          <Box bg="#00ca4e" borderRadius="10rem" w="10px" h="10px" mr={1} />
         </Box>
-        <Box
-          h="300px"
-          borderBottomLeftRadius="0.25rem"
-          borderBottomRightRadius="0.25rem"
-          bg="#00274cdd"
-          textAlign="left"
-          p={1}
-        >
-          {commands.map((command, index) => {
-            return <Command key={command.command} command={command} />;
-          })}
-
-          <Text my={0} fontWeight={500} fontSize="sm" fontFamily="monospace">
-            <span style={{ color: "#fff", paddingRight: "5px" }}>
-              {`⭢ ${commands[commands.length - 1].dir}`}
-            </span>
-          </Text>
-        </Box>
+        <Box />
+        <Text color="white" fontFamily="monospace" my={1} userSelect="none">
+          {title}
+        </Text>
+        <Box />
       </Box>
-    </Draggable>
+      <Box
+        borderBottomLeftRadius="0.25rem"
+        borderBottomRightRadius="0.25rem"
+        bg="#00274cdd"
+        textAlign="left"
+        p={1}
+      >
+        {commands.map((command, index) => {
+          return <Command key={command.command} command={command} />;
+        })}
+
+        <Text my={0} fontWeight={500} fontSize="sm" fontFamily="monospace">
+          <span style={{ color: "#fff", paddingRight: "5px" }}>
+            {`⭢ ${commands[commands.length - 1].dir}`}
+          </span>
+        </Text>
+      </Box>
+    </Rnd>
   );
 };
